6b4cf3e25ff6942b0752521c32cf09c75e403d8e
Sidharrth <sidharrth2002@gmail.com>
2020-12-01 23:39:43 +0800

    feat: changes role after url verification

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.utils import get
 
 import urllib.request
 import urllib.parse
@@ -24,7 +25,8 @@ async def on_ready():
     memberlist = [member.name for member in guild.members]
     print(f'Guild members: \n - {memberlist}')
 
-@bot.command(name='verify')
+
+@bot.command(name='verify', pass_context=True)
 async def verify(ctx):
     await ctx.send("Let's go ahead and do some quick verification so I can admit you into the server.")
     await ctx.send("Enter any social media URL")
@@ -36,12 +38,19 @@ async def verify(ctx):
     await ctx.send("Let me verify that for you")
 
     url = msg.content
-    status_code = urllib.request.urlopen(url).getcode()
+    try:
+        status_code = urllib.request.urlopen(url).getcode()
+        valid_page = status_code == 200
+        if (valid_page):
+            await ctx.send('You have been verified. You will be given member access.')
+            member = ctx.message.author
+            role = get(member.guild.roles, name='member')
+            print(role)
+            await member.add_roles(role)
 
-    valid_page = status_code == 200
-    if (valid_page):
-        await ctx.send('You have been verified. You will be given member access')
-
+    except Exception as e:
+        print(e)
+        await ctx.send("I couldn't find an account on that particular page. Let's retry the verification. Call me again using !verify.")
     
 @bot.event
 async def on_member_join(member):
